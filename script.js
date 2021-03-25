49469047ad0a9dddf6aba0bf18186d1d58764f9a
Alexandre Souza <alexandrehg2001@gmail.com>
2021-03-25 17:48:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function certo(){
+    document.getElementById('pokeProf').style.filter = "brightness(1)";
+    alert('PARABÉNSSSS VC ACHO O PAULINHO CEO DA ALURA');
+}
+  
+function errado(){ document.getElementById('pokeProf').style.filter = "brightness(0)";
+    alert('puts vc erro man :/');
+}
+  
